f14620df4d4bc45f1c7bfedf2f8fdfafe01cba4b
Allan Marques <graftual@gmail.com>
2022-04-27 15:37:59 -0300

    update groupId

--- src/main/java/com/github/allanmarques83/DotEnv.java
+++ src/main/java/com/github/allanmarques83/DotEnv.java
@@ -1,4 +1,4 @@
-package com.dotenv;
+package com.github.allanmarques83;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
--- src/main/java/com/github/allanmarques83/DotEnvConfiguration.java
+++ src/main/java/com/github/allanmarques83/DotEnvConfiguration.java
@@ -1,4 +1,4 @@
-package com.dotenv;
+package com.github.allanmarques83;
 
 import java.io.IOException;
 import java.nio.file.Files;
